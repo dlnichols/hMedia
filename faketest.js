f8e941ca2217547b1bcf79db75ed423936c7b879
Dan Nichols <dan@thenicholsrealm.com>
2014-03-04 18:41:25 -0600

    Clean up some things we don't use from the yeoman build

--- faketest.js
+++
@@ -1,5 +0,0 @@
-var faker = require('Faker');
-console.log(Math.floor(Math.random()*16777215).toString(16) + Math.floor(Math.random()*16777215).toString(16));
-
-console.log(faker.Helpers.randomize("asdasdasdasasdasdasdasdasda"));
-console.log(faker.Company.catchPhrase());
